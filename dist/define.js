1e5bc84cf4943fb1b58887c6ecee876f91ee7575
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2021-04-06 13:23:02 +0700

    v1.3.4

--- dist/define.js
+++ dist/define.js
@@ -1,2 +1,2 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports["default"]=void 0;var Define={};var _default=Define;exports["default"]=_default;
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;var Define={};var _default=Define;exports.default=_default;
 //# sourceMappingURL=define.js.map
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 export {default} from './install'
 export {default as DefineManager} from './define_manager'
 export {default as Define} from './define'
